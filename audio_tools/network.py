04bd0c01ae06f18085bb2220f31303e7218d366d
Sepand KASHANI <kashani.sepand@gmail.com>
2021-08-30 17:50:26 +0200

    [network] PacketServer: partial shell.

--- audio_tools/network.py
+++ audio_tools/network.py
@@ -1,4 +1,5 @@
 import queue
+import threading
 
 import numpy as np
 
@@ -7,9 +8,27 @@ import audio_tools.interface as ati
 
 class PacketServer(ati.PacketStream):
     """
-    Route packets from a PacketStream to different parties through a TCP interface.
+    Route packets from a PacketStream to several PacketClients through a TCP interface.
 
     This class is designed to dispatch available packets to each destination as soon as possible.
+
+    Architecture
+    ------------
+    PacketServer is multi-threaded to efficiently connect the local PacketStream to remote
+    PacketClients via:
+    * A NetworkListener process which monitors all incoming connection requests;
+    * A LocalStreamer process which fan-outs local packets to NetworkStreamer processes; and
+    * NetworkStreamer processes (one per PacketClient) which transmit local packets to remote consumers.
+
+    When receiving a new connection request, the NetworkListener spawns a new NetworkStreamer to
+    handle the connection. Shared ressources are accessed through a lock when required.
+
+    Note
+    ----
+    * PacketServer.stop() is not instantaneous due to internal timeouts to avoid thread deadlocks.
+    * Some NetworkStreamers may not end after calling PacketServer.stop(). This can happen if the
+      remote connection was not made through a PacketClient. This is a non-issue: the OS will
+      force-close such zombie connections after a while.
     """
 
     def __init__(self, stream: ati.PacketStream, port: int):
@@ -22,17 +41,45 @@ class PacketServer(ati.PacketStream):
             TCP listening port.
         """
         super().__init__(dtype=None, sample_rate=None)  # not yet known
-        # TODO
         self._stream = stream
         self._port = port
 
+        self._thread: dict[int, threading.Thread] = {}
+        self._pkt_q: dict[int, queue.Queue] = {}
+        self._q_lck = threading.Lock()  # thread synchronization to modify [_thread | _pkt_q]
+        self._active = threading.Event()  # thread start/stop notification
+
     def start(self):
-        # TODO
-        raise NotImplementedError
+        if self.active():
+            pass
+        else:
+            if not self._stream.active():  # initiate acquisition
+                self._stream.start()
+            self._dtype = self._stream.dtype
+            self._sample_rate = self._stream.sample_rate
+            self._active.set()
+
+            self._thread = {
+                0: PacketServer._LocalStreamer(self),
+                1: PacketServer._NetworkListener(self),
+            }
+            for t in self._thread.values():
+                t.start()
 
     def stop(self):
-        # TODO
-        raise NotImplementedError
+        self._stream.stop()  # stop acquisition
+        self._stream.clear()
+
+        self._active.clear()  # gracefully terminate threads
+        for t in self._thread.values():
+            t.join()
+        self._thread = dict()
+
+        for q in self._pkt_q.values():  # and dispose of any lingering state.
+            while not q.empty():
+                q.get()
+                q.task_done()
+        self._pkt_q = dict()
 
     def get(self) -> np.ndarray:
         raise NotImplementedError("Operation not supported.")
@@ -42,6 +89,18 @@ class PacketServer(ati.PacketStream):
 
     def __len__(self) -> int:
         raise NotImplementedError("Operation not supported.")
+
+    def active(self) -> bool:
+        return self._active.is_set()
+
+    class _LocalStreamer(threading.Thread):
+        pass
+
+    class _NetworkListener(threading.Thread):
+        pass
+
+    class _NetworkStreamer(threading.Thread):
+        pass
 
 
 class PacketClient(ati.PacketStream):
